39d98700eecdddf184bf8c94c31eec7424987054
Gabriel <106531227+gabrielti104@users.noreply.github.com>
2023-08-28 22:13:07 -0300

    Add files via upload

---
+++ lojacarros/src/main/java/com/mycompany/lojacarros/Lojacarros.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lojacarros;
+
+/**
+ *
+ * @author gabriel.ssilva155
+ */
+public class Lojacarros {
+
+    public static void main(String[] args) {
+        Telalogin login = new Telalogin();
+        login.setVisible(true);
+    }
+}
